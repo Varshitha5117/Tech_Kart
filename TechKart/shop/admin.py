5d497448ebce8e8b9a33b793d5436df1b26a4b76
Rozal12 <Rozal12@gmail.com>
2025-05-26 00:52:51 +0530

    adminsite

--- TechKart/shop/admin.py
+++ TechKart/shop/admin.py
@@ -1,5 +1,5 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Product
-#Here we can register projects
+#Here we can register projects to the admin site
 admin.site.register(Product)
